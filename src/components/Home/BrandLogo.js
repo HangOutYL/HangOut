0c0bf5440aa62ec3ad24305e3e0ba78bdec9df51
Shimesh <yaron.orb@gmail.com>
2021-12-16 15:10:20 +0200

    Cafe Hangout Brand Logo Design

--- src/components/Home/BrandLogo.js
+++ src/components/Home/BrandLogo.js
@@ -3,8 +3,8 @@ import React from "react";
 const BrandLogo = () => {
   return (
     <div className="Logo">
-      <span className="brand">Hang</span>
-      <span className="logo">Out</span>
+      <span className="brand">Cafe</span>
+      <span className="logo">HangOut</span>
     </div>
   );
 };
