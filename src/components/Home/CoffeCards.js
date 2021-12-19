ce6c774974d69f61df8c7fa1a1fecc4edcc5cb66
Shimesh <yaron.orb@gmail.com>
2021-12-19 14:55:40 +0200

    Coffee Cards Layout Design

--- src/components/Home/CoffeCards.js
+++ src/components/Home/CoffeCards.js
@@ -1,7 +1,59 @@
 import React from "react";
+import Coffee1 from "../../views/CoffeeImage1.png";
 
 const CoffeCards = () => {
-  return <div className="CoffeeCards">Coffe Cards Component</div>;
+  return (
+    <div className="CoffeeCards">
+      <div className="Card">
+        <img src={Coffee1} alt="CoffeeImage" />
+        <span className="Text">Some Text</span>
+        <div className="PriceAndCart">
+          <span className="Price">1.99$</span>
+          <button className="AddToCart">+</button>
+        </div>
+      </div>
+      <div className="Card">
+        <img src={Coffee1} alt="CoffeeImage" />
+        <span className="Text">Some Text</span>
+        <div className="PriceAndCart">
+          <span className="Price">1.99$</span>
+          <button className="AddToCart">+</button>
+        </div>
+      </div>
+      <div className="Card">
+        <img src={Coffee1} alt="CoffeeImage" />
+        <span className="Text">Some Text</span>
+        <div className="PriceAndCart">
+          <span className="Price">1.99$</span>
+          <button className="AddToCart">+</button>
+        </div>
+      </div>
+      <div className="Card">
+        <img src={Coffee1} alt="CoffeeImage" />
+        <span className="Text">Some Text</span>
+        <div className="PriceAndCart">
+          <span className="Price">1.99$</span>
+          <button className="AddToCart">+</button>
+        </div>
+      </div>
+      <div className="Card">
+        <img src={Coffee1} alt="CoffeeImage" />
+        <span className="Text">Some Text</span>
+        <div className="PriceAndCart">
+          <span className="Price">1.99$</span>
+          <button className="AddToCart">+</button>
+        </div>
+      </div>
+      <div className="Card">
+        <img src={Coffee1} alt="CoffeeImage" />
+        <span className="Text">Some Text</span>
+        <div className="PriceAndCart">
+          <span className="Price">1.99$</span>
+          <button className="AddToCart">+</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoffeCards;
