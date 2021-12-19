c7c58b8d57f2b3bad6371254ef60ff681c9c42b2
Shimesh <yaron.orb@gmail.com>
2021-12-19 12:13:03 +0200

    Scaling Down View Size, added Button Nav Icons

--- src/components/Home/ButtomNav.js
+++ src/components/Home/ButtomNav.js
@@ -1,7 +1,14 @@
 import React from "react";
+import HomeButton from "../../views/HomeButtonVector.png";
+import CartButton from "../../views/CartButtonVector.png";
 
 const ButtomNav = () => {
-  return <div className="ButtomNav">This Is Buttom Nav Bar</div>;
+  return (
+    <div className="ButtomNav">
+      <img src={HomeButton} alt="HomeButton" />
+      <img src={CartButton} alt="CartButton" />
+    </div>
+  );
 };
 
 export default ButtomNav;
