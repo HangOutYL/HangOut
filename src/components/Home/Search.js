65027149762ae4e98cfe589d593b256f1fb9fc56
Shimesh <yaron.orb@gmail.com>
2021-12-19 15:36:20 +0200

    Search Bar Design

--- src/components/Home/Search.js
+++ src/components/Home/Search.js
@@ -1,7 +1,13 @@
 import React from "react";
+import SearchIcon from "../../views/SearchVector.png";
 
 const Search = () => {
-  return <div className="SearchBar">This is The Search Bar</div>;
+  return (
+    <div className="SearchBar">
+      <img src={SearchIcon} alt="SearchIcon" />
+      <input placeholder="Search your favourite Coffee.." />
+    </div>
+  );
 };
 
 export default Search;
