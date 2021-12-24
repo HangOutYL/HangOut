1d7d001bdeee66daa4e32cab9cbbee34e8abfe4c
lisim <lior443@gmail.com>
2021-12-24 16:31:08 +0200

    built Product and Products components with css

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Product from "./Product/Product";
+import "./Products.css";
 
 const Products = () => {
   return (
-    <>
+    <div className="CoffeeCards">
       <Product />
-    </>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
   );
 };
 
