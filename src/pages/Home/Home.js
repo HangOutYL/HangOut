d0354d77886816e1f8f81c99cef7c7cea4e13cd8
lisim <lior443@gmail.com>
2021-12-27 21:17:49 +0200

    moved bottomav component in home component below

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import BottomNav from "../../components/BottomNav/BottomNav";
 import BrandLogo from "./BrandLogo";
 import Categories from "./Categories";
-// import CoffeeCards from "./CoffeeCards";
 import Products from "../../components/Products/Products";
 import "./home.css";
 import LoggedUser from "./LoggedUser";
@@ -10,15 +9,16 @@ import Search from "./Search";
 
 const Home = () => {
   return (
-    <div className="Home">
-      <BrandLogo />
-      <LoggedUser />
-      <Search />
-      {/* <CoffeeCards /> */}
-      <Products />
-      <Categories />
+    <>
+      <div className="Home">
+        <BrandLogo />
+        <LoggedUser />
+        <Search />
+        <Products />
+        <Categories />
+      </div>
       <BottomNav />
-    </div>
+    </>
   );
 };
 
