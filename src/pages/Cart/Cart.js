59e2d80bd77fea85112cfe83a5af7057ba9394dc
lisim <lior443@gmail.com>
2021-12-27 19:41:29 +0200

    changed css file to the previous design

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Coffee1 from "../../views/CoffeeImage1.png";
-import CouponCode from "../../views/Coupon2svg.svg";
-import DashedLine from "../../views/DashedLine2svg.svg";
+import CouponCode from "../../views/CouponVector.png";
+import DashedLine from "../../views/DashedLineVector.png";
 import BottomNav from "../../components/BottomNav/BottomNav";
 import "./Cart.css";
 
@@ -26,21 +26,6 @@ const Cart = () => {
               <button className="Item-Plus">+</button>
             </div>
           </div>
-          <div className="Item">
-            <div className="Item-Desc">
-              <img src={Coffee1} alt="item-img" className="item-img" />
-              <div className="Item-Detail">
-                <span className="Item-Title">Cappuccino</span>
-                <span className="Item-Text">Dalgona Macha</span>
-                <span className="Item-Price">1.99$</span>
-              </div>
-            </div>
-            <div className="Item-Amount">
-              <button className="Item-Minus">-</button>
-              <span className="Item-Number">0</span>
-              <button className="Item-Plus">+</button>
-            </div>
-          </div>
 
           <div className="Item">
             <div className="Item-Desc">
@@ -74,7 +59,7 @@ const Cart = () => {
             </div>
           </div>
         </div>
-        <img src={DashedLine} alt="DashLine" className="DashLine1" />
+        <img src={DashedLine} alt="DashLine" className="DashLine" />
         <div className="Coupon">
           <img src={CouponCode} alt="Cart-Coupon" className="Cart-Coupon" />
         </div>
@@ -89,7 +74,7 @@ const Cart = () => {
               <span className="Taxes-Price">3.46$</span>
             </div>
           </div>
-          <img src={DashedLine} alt="DashLine" className="DashLine2" />
+          <img src={DashedLine} alt="DashLine" className="DashLine" />
           <div className="Order-Total">
             <span className="Grand-Total">Grand Total</span>
             <span className="Total-Price">21.99$</span>
