872df375f5cea6a9091ee32d0794c7b42c2f6845
lisim <lior443@gmail.com>
2021-12-29 01:28:11 +0200

    fixed css with bottomnav, home, productdetail.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import BottomNav from "../../components/BottomNav/BottomNav";
 import BrandLogo from "./BrandLogo";
 import Categories from "./Categories";
 import Products from "../../components/Products/Products";
-import "./home.css";
+import "./Home.css";
 import LoggedUser from "./LoggedUser";
 import Search from "./Search";
 
