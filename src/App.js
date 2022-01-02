ad7ad2d73716394113f8dedca37689d75b5d5fed
Shimesh <yaron.orb@gmail.com>
2022-01-02 20:15:52 +0200

    Fetching Data From JSON using UseEffect and UseState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./pages/Cart/Cart";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
 import Home from "./pages/Home/Home";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`http://localhost:3000/data.json`);
+      const products = await res.json();
+      console.log(products);
+      setData(() => [...data, products]);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="app">
       <Routes>
