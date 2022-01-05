05ae2fdf69c84ed093bea69a9d14941653e4f718
Shimesh <yaron.orb@gmail.com>
2022-01-05 17:29:42 +0200

    category change

--- src/pages/Home/Categories.js
+++ src/pages/Home/Categories.js
@@ -2,17 +2,14 @@ import React from "react";
 
 const Categories = ({ names }) => {
   const catNames = names;
-
   console.log(catNames);
   return (
     <div className="CoffeeCategories">
       {catNames?.map((cat) => (
-        <div className="Category">{cat}</div>
+        <div className="Category" key={cat}>
+          {cat}
+        </div>
       ))}
-      {/* <div className="Category">Category</div>
-      <div className="Category">Category</div>
-      <div className="Category">Category</div>
-      <div className="Category">Category</div> */}
     </div>
   );
 };
