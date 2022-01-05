b59f446ef497afbc7d679755ec953e6a8f59cb61
lisim <lior443@gmail.com>
2022-01-05 19:48:35 +0200

    productdetails fetch from data

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import ArrowBack from "../../views/ArrowBackVector.png";
-import Coffee1Big from "../../views/CoffeeImage1Big.png";
+// import Coffee1Big from "../../views/CoffeeImage1Big.png";
 import "./ProductDetails.css";
 
 const ProductDetails = () => {
@@ -11,9 +11,7 @@ const ProductDetails = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(
-        `https://api.npoint.io/4b5384c6f07519b8aaa6/${id}`
-      );
+      const res = await fetch(`http://localhost:8080/coffees/${id}`);
       const data = await res.json();
       setProduct(data);
     };
@@ -21,44 +19,42 @@ const ProductDetails = () => {
   }, [id]);
 
   return (
-    <div className="Product">
-      <img src={Coffee1Big} alt="CoffeeImage" className="BigPicture" />
-      <Link to={"/"}>
-        <img src={ArrowBack} alt="ArrowBack" className="ArrowBack" />
-      </Link>
-      <div className="NameAndDesc">
-        <span className="ProductName">Cappuccino</span>
-        <span className="ProductDetails">Drizzled with Caramel</span>
-        <span className="ProductDesc">
-          A single espresso shot poured into hot foamy milk, with the surface
-          topped with mildly sweetened cocoa powder and drizzled with
-          scrumptious caramel syrup ... Read More
-        </span>
-      </div>
-      <div className="MoreOptions">
-        <span className="MoreOptionsTitle">Choice Of Milk</span>
-        <div className="Tabs">
-          <div className="Tab">
-            <button className="TabText">Oat Milk</button>
-          </div>
-          <div className="Tab">
-            <button className="TabText">Soy Milk</button>
-          </div>
-          <div className="Tab">
-            <button className="TabText">Almond Milk</button>
+    product && (
+      <div className="Product">
+        <img src={product.image} alt="CoffeeImage" className="BigPicture" />
+        <Link to={"/"}>
+          <img src={ArrowBack} alt="ArrowBack" className="ArrowBack" />
+        </Link>
+        <div className="NameAndDesc">
+          <span className="ProductName">{product.category}</span>
+          <span className="ProductDetails">{product.title}</span>
+          <span className="ProductDesc">{product.description}</span>
+        </div>
+        <div className="MoreOptions">
+          <span className="MoreOptionsTitle">Choice Of Milk</span>
+          <div className="Tabs">
+            <div className="Tab">
+              <button className="TabText">Oat Milk</button>
+            </div>
+            <div className="Tab">
+              <button className="TabText">Soy Milk</button>
+            </div>
+            <div className="Tab">
+              <button className="TabText">Almond Milk</button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="PriceAndBuy">
-        <div className="Price">
-          <span className="PriceText">Price</span>
-          <span className="PriceAmount">299$</span>
+        <div className="PriceAndBuy">
+          <div className="Price">
+            <span className="PriceText">Price</span>
+            <span className="PriceAmount">{product.price}</span>
+          </div>
+          <button className="BuyNowBtn">
+            <span className="BuyBtnText">BUY NOW</span>
+          </button>
         </div>
-        <button className="BuyNowBtn">
-          <span className="BuyBtnText">BUY NOW</span>
-        </button>
       </div>
-    </div>
+    )
   );
 };
 
