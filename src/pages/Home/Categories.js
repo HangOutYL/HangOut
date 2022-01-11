0fabeada1346fd2fc39aa15959634738a7937c4f
Shimesh <yaron.orb@gmail.com>
2022-01-11 14:28:21 +0200

    Adding an "ALL" products Button

--- src/pages/Home/Categories.js
+++ src/pages/Home/Categories.js
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
+import { useState } from "react/cjs/react.development";
 import ProductsContext from "../../Context/ProductsContext";
 
 const Categories = ({ names }) => {
+  const [all, setAll] = useState(false);
   const { products, searchedProducts, setSearchedProducts } =
     useContext(ProductsContext);
   const catNames = names;
@@ -14,10 +16,21 @@ const Categories = ({ names }) => {
     filteredCategory === searchedProducts
       ? setSearchedProducts(products)
       : setSearchedProducts(filteredCategory);
+    setAll(true);
+  };
+
+  const toggleALL = (e) => {
+    setSearchedProducts(products);
+    setAll(false);
   };
 
   return (
     <div className="CoffeeCategories">
+      {all && (
+        <button value="all" className="all" onClick={toggleALL}>
+          ALL
+        </button>
+      )}
       {catNames?.map((cat, key) => (
         <button
           className="Category"
