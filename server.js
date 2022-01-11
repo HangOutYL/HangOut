354d66d957ffa312470aab694476b222c5590947
Shimesh <yaron.orb@gmail.com>
2022-01-11 21:56:50 +0200

    Server.js initial structure

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello From Server");
 });
+app.get("/", (req, res) => {});
+app.post("/", (req, res) => {});
+app.put("/", (req, res) => {});
+app.delete("/", (req, res) => {});
 
 app.listen(PORT, () => console.log(`Server is listening on PORT${PORT}`));
