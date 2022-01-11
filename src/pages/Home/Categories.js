1e69967333fef7af753811e1262ea420590fe44d
Shimesh <yaron.orb@gmail.com>
2022-01-11 11:45:14 +0200

    Filter By Category

--- src/pages/Home/Categories.js
+++ src/pages/Home/Categories.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import ProductsContext from "../../Context/ProductsContext";
 
 const Categories = ({ names }) => {
+  const { searchedProducts, setSearchedProducts } = useContext(ProductsContext);
+
   const catNames = names;
+
+  const handleClick = (e) => {
+    const clickedCategory = e.target.value;
+    const filteredCategory = searchedProducts?.filter(
+      (product) => product.category === clickedCategory
+    );
+    console.log(filteredCategory);
+
+    setSearchedProducts(filteredCategory);
+  };
+
   return (
     <div className="CoffeeCategories">
-      {catNames?.map((cat) => (
-        <div className="Category" key={cat}>
+      {catNames?.map((cat, key) => (
+        <button
+          className="Category"
+          key={key}
+          value={cat}
+          onClick={(e) => handleClick(e)}
+        >
           {cat}
-          {console.log({ cat })}
-        </div>
+        </button>
       ))}
     </div>
   );
