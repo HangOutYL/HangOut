cb2fecacce98866cc036f2b45c06cc15fac32375
Shimesh <yaron.orb@gmail.com>
2022-01-12 10:34:43 +0200

    import mongoose

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from "express";
+import connect from "mongoose";
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello From Server");
+  res.status(200).send("Hello From Server");
 });
 app.get("/", (req, res) => {});
 app.post("/", (req, res) => {});
 app.put("/", (req, res) => {});
 app.delete("/", (req, res) => {});
 
-app.listen(PORT, () => console.log(`Server is listening on PORT${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is listening on "http://localhost:${PORT}"`)
+);
