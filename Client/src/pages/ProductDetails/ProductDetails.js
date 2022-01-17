eb2974adc45c53f62048ff5ad2f8b3fd8e94d4cc
lisim <lior443@gmail.com>
2022-01-17 19:11:39 +0200

    added milk type function

--- Client/src/pages/ProductDetails/ProductDetails.js
+++ Client/src/pages/ProductDetails/ProductDetails.js
@@ -18,6 +18,13 @@ const ProductDetails = () => {
     fetchData();
   }, [id]);
 
+  const milkChoice = (e) => {
+    const { milk } = product;
+    if (!milk.length) {
+      milk.push(e.target.value);
+    }
+  };
+
   return (
     product && (
       <div className="Product">
@@ -36,20 +43,26 @@ const ProductDetails = () => {
           <span className="MoreOptionsTitle">Choice Of Milk</span>
           <div className="Tabs">
             <div className="Tab">
-              <button className="TabText">Oat Milk</button>
+              <button className="TabText" value={"oat"} onClick={milkChoice}>
+                Oat Milk
+              </button>
             </div>
             <div className="Tab">
-              <button className="TabText">Soy Milk</button>
+              <button className="TabText" value={"soy"} onClick={milkChoice}>
+                Soy Milk
+              </button>
             </div>
             <div className="Tab">
-              <button className="TabText">Almond Milk</button>
+              <button className="TabText" value={"almond"} onClick={milkChoice}>
+                Almond Milk
+              </button>
             </div>
           </div>
         </div>
         <div className="PriceAndBuy">
           <div className="Price">
             <span className="PriceText">Price</span>
-            <span className="PriceAmount">{product.price}</span>
+            <span className="PriceAmount">${product.price}</span>
           </div>
           <button className="BuyNowBtn">
             <span className="BuyBtnText">BUY NOW</span>
