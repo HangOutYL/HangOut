6b2ed48347ae1bd10801de72facf69b96d990bb9
Shimesh <yaron.orb@gmail.com>
2022-01-24 11:36:26 +0200

    MongoDB Connection + Server Update + Schema + .env

---
+++ models/products.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  milk: {
+    type: Array,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Products", productSchema);
--- server.js
+++ server.js
@@ -1,25 +1,87 @@
 import express from "express";
 import fetch from "node-fetch";
 import mongoose from "mongoose";
+import Products from "./models/products.js";
 import cors from "cors";
 import dotenv from "dotenv";
 const app = express();
-const PORT = 8080;
 
 dotenv.config();
-// Middlewares
+
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+
+// Getting Initial Products
+async function initProducts() {
+  const productsDb = await Products.find();
+  if (!productsDb.length) {
+    const res = await fetch("https://api.npoint.io/ac214e395851de88f4f6");
+    const data = await res.json();
+    const mapped = data.map((p) => ({ ...p }));
+    await Products.insertMany(mapped, () => {
+      console.log("successfully Populated Products DB");
+    });
+  }
+}
+
+// Express Middlewares
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello From Server");
-});
-app.get("/", (req, res) => {});
-app.post("/", (req, res) => {});
-app.put("/", (req, res) => {});
-app.delete("/", (req, res) => {});
+// Express Routing
 
-app.listen(PORT, () =>
-  console.log(`Server is listening on "http://localhost:${PORT}"`)
+app.get("/api/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Products.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Products.findById(id);
+  res.status(200).send(product);
+});
+
+app.post("/api/products", async (req, res) => {
+  const body = req.body;
+  const product = await Products.create({ ...body });
+  res.status(200).send(product);
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Products.findByIdAndRemove(id);
+  res.status(200).send(product);
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const product = await Products.findByIdAndUpdate(id, { ...body });
+  const updatedProduct = await Products.findById(id);
+  res.status(200).send(updatedProduct);
+});
+
+app.get("*", (req, res) => {
+  res.status(200).send("*");
+});
+
+// Mongoose Connection To DB
+
+mongoose.connect(
+  `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+  () => {
+    app.listen(process.env.PORT || 8080, () => {
+      initProducts();
+
+      console.log("Server up and running");
+    });
+  }
 );
