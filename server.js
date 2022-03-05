dd2ee18e8cd57ac2cc197ea4ea4ce8fd18019394
Shimesh <yaron.orb@gmail.com>
2022-03-05 20:13:39 +0200

    public folder try

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.delete("/api/users/logout", async (res, req) => {
 
 const path = require("path");
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/public", "index.html"));
 });
 
 // app.get("*", (req, res) => {
